7e5170d93aa62c72ca60873426c9b678b2c9b957
vipinrao009 <vipinrao2018@gmail.com>
2024-05-05 16:34:00 +0530

    Integrated Gmail with sendEmail

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -5,16 +5,21 @@ const sendEmail = async function (email, subject, message) {
   let transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: false,
+    secure: true,
     auth: {
       user: process.env.SMTP_USERNAME,
       pass: process.env.SMTP_PASSWORD,
     },
   });
 
+    const companyName = "LMS SYSTEM";
+
+  // Set the "from" field to include the company name
+  const fromField = `"${companyName}" <${process.env.SMTP_FROM_EMAIL}>`;
+
   //   send mail with defined transport object
   await transporter.sendMail({
-    from: process.env.SMTP_FROM_EMAIL, //sender email
+    from: fromField, //sender email
     to: email, // user email
     subject: subject, // Subject line
     html: message, // html body
