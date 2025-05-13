f2dae73f379f582c8a211d947a8977ea98337dca
vipinrao009 <vipinrao2018@gmail.com>
2025-05-13 14:24:49 +0530

    Remove localhost from CORS origin configuration

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL,"http://localhost:5173"],
+    origin: [process.env.FRONTEND_URL],
     credentials: true
   })
 );
